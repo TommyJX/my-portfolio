a9a28c3890a86790a52a34fbbb619e011997739f
Tommy Wang <ttommywang@gmail.com>
2023-12-20 20:43:17 +0100

    added scroll function
    in order to make it work when the user clicks on it

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tablinks = document.getElementsByClassName("tab-links")
 const tabcontents = document.getElementsByClassName("tab-contents")
+let mybutton = document.getElementById("myBtn")
 
 function opentab(tabname) {
     for (tablink of tablinks) {
@@ -13,4 +14,19 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link")
 
     document.getElementById(tabname).classList.add("active-tab")
+}
+
+/*-----------scroll-----------*/
+window.onscroll = function () { scrollFunction() }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block"
+    } else {
+        mybutton.style.display = "none"
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0
 }
