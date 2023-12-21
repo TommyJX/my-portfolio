4511695a88edab70538a51d7500ca3176aadcb59
Tommy Wang <ttommywang@gmail.com>
2023-12-21 18:48:42 +0100

    added function on menu for phone users

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ function opentab(tabname) {
 }
 
 
+/*-----------menu-----------*/
+const sidemenu = document.getElementById("sidemenu")
+
+function openmenu() {
+    sidemenu.style.right = "0"
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px"
+}
+
+
 
 /*-----------scroll-----------*/
 let calcScrollValue = () => {
